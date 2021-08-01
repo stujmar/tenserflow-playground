77064e3b7b2caaa63d7c6231dbedd6aa2bc70c58
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-01 14:12:22 -0500

    [sm] created some meaningless single data point tensors

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import cProfile
 print(tf.version)
 tf.executing_eagerly()
 print('tesnorflow executed')
+
+# Creating Tensors
+string = tf.Variable("this is a string", tf.string)
+integer = tf.Variable(123, tf.int16)
+float = tf.Variable(3.21, tf.float32)
