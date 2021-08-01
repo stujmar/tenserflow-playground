9d7207d83015a3519abfd6a09bee6d10fdb9243f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-01 14:17:31 -0500

    [sm] did a rank 1 and rank 2 tewnsor and printed a checking of the rank of the tensors

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ print(tf.version)
 tf.executing_eagerly()
 print('tesnorflow executed')
 
-# Creating Tensors
+# Creating Tensors (Rank 0)
 string = tf.Variable("this is a string", tf.string)
 integer = tf.Variable(123, tf.int16)
 float = tf.Variable(3.21, tf.float32)
+
+# Rank/Degree of Tensors
+rank1_tensor = tf.Variable(["Test", "Data", "Here"], tf.string)
+rank2_tensor = tf.Variable([["rank", "one"],["rank", "two"]], tf.string)
+
+print("checking rank")
+print(tf.rank(rank2_tensor))
