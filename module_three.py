4fb52e3398314f5387239445d3c1b5e42c31255b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-05 21:20:29 -0500

    [sm] got module_three running but graphs not showing up

--- module_three.py
+++ module_three.py
@@ -1,9 +1,15 @@
-import os
-import tensorflow as tf
-import cProfile
+# import os
+# import tensorflow as tf
+# import cProfile
 
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import numpy as np
+
+x = [1, 2, 2.5, 3, 4]
+y = [1, 4, 7, 9, 15]
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
+print('module_three')
 
 # Algorithm Types
 
