c7dadbfb62cd896642e110ed2ec09f845dbe981e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-06 21:06:50 -0500

    [sm] added indicaters of which colors I want the lines to be on the plots

--- plotting.py
+++ plotting.py
@@ -9,14 +9,16 @@ dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 7
 py_dev_y = [45372, 48876, 53850, 57287, 63016,
             65998, 70003, 70000, 71496, 75370, 83640]
 
-plt.plot(ages_x, dev_y, label='All Devs')
-plt.plot(ages_x, py_dev_y, label='Python')
+plt.plot(ages_x, dev_y, color='b', label='All Devs')
+plt.plot(ages_x, py_dev_y, color='g', label='Python')
 
 
 plt.xlabel('Ages')
 plt.ylabel('Median Salary (USD)')
 plt.title('Median Salary (USD) by Age')
 plt.legend()
+
+# You can pass all the lengend info like this.
 # plt.legend(['All Devs', 'Python Devs'])
 
 plt.show()
