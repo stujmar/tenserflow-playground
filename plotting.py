77357ff81e01c312feb087013bc67981d614746e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-06 21:12:23 -0500

    [sm] added data for specifically JS devs to the plot and styled it blue

--- plotting.py
+++ plotting.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 
 ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
 
-
-
-py_dev_y = [45372, 48876, 53850, 57287, 63016,
-            65998, 70003, 70000, 71496, 75370, 83640]
 
 plt.plot(ages_x, dev_y, color='#e7b417', marker='.', label='All Devs')
 plt.plot(ages_x, py_dev_y, color='#3a9c8f', marker='.', label='Python')
+plt.plot(ages_x, js_dev_y, color='#16c3fc', marker='.', label='Javascript')
 
 
 plt.xlabel('Ages')
