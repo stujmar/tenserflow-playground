da0d0e5c183aef0645c8c9613a752fa43c3e5878
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-13 22:36:18 -0500

    [sm] tried a print tweets but got some bad authentication data

--- test_print.py
+++ test_print.py
@@ -1,1 +1,16 @@
+import requests
+
 print('test working')
+
+# Function to get most favortied tweets of user
+def get_favorites(username):
+    url = 'https://api.twitter.com/1.1/favorites/list.json'
+    params = {'screen_name': username, 'count': 5}  
+    response = requests.get(url, params=params)
+    return response.json()
+
+# print the most favorited tweets of user
+for tweet in get_favorites('stujmar'):
+    print(tweet)
+
+print(get_favorites('stujmar'))
