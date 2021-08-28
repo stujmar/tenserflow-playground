c07e84b64e1f88c95ca3cab45a59233806b2bc2d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-28 13:17:27 -0500

    [sm] did the first 10 minutes of anumpy tutorial and took notes in the numpy_tests.py

--- numpy_tests.py
+++ numpy_tests.py
@@ -1,3 +1,18 @@
 import numpy as np
+import sys
 
+# NumPy is a multi-dimensional array object, similar to a list.
+# NumPy is much faster than Python lists.
+
+# Int32 holds 4 bytes, or 32 bits. 00000000 00000000 00000000 00000000
+# Int8 holds 1 byte, or 8 bits. 00000000
+
+# Python list hold Size, Reference, Value, Object Type. While Numpy only hold values.
+# Python list is mutable, while Numpy is not? Fact check this is it from github copilot.
+
+# Numpy is contiguous (all values in one place). Python list is not.
+# a Python list is a bunch of pointers to where the data is stored.
+
+# Numpy can to additions deletions etc as can Python lists, but Numpy can do much more.
+# Mathlab Pandas and Scipy are other packages that use Numpy? double check this.
 
