5dc700d6d179c912a201f29a443975459c731c4a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-01 23:01:34 -0500

    [sm] did the first lession in the Deep Learning Fundamentals video

--- bourke.py
+++ bourke.py
@@ -1,3 +1,13 @@
 # Starting a TensorFlow Tutorial from
 # https://www.youtube.com/watch?v=tpCFfeUEGs8
 
+# What is deep learning?
+# Multiple layers of processing to extract information from data.
+# Machine learning is data into numbers and pulling out patterns.
+
+# (AI(ML(NN))) (AI(ML(DL))) Neural Nets, Deep Learning, within Machine Learning.
+# Provide Data and Outcome and ML/DL will provide the rules/path to get there.
+
+# Example with a self driving car all the rules would be too much to type.
+# If you can build a simple rule based system don't use ML.
+# Google's ML guide https://developers.google.com/machine-learning/guides
