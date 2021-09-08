30d9268a498bcd16eb06e9ca0f45f02f8281472c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-07 20:50:24 -0500

    [sm] created a loop and formatted message to test our radii within the circle.py

--- testing/circle.py
+++ testing/circle.py
@@ -8,4 +8,9 @@ def circle_area(r):
 # Good check
 
 radii = [2, 0, -3, 2 + 5j, True, "string"]
+message = "Area of circles with r= {radius} is {area}"
+
+for r in radii:
+    A = circle_area(r)
+    print(message.format(radius=r, area=A))
 
