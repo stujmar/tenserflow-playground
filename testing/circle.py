1fde9c97327bce8c9d634d893d8fd7a9b239b5a8
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-07 20:54:27 -0500

    [sm] cleaned out crude testing from circle.py and imported method pi and unit test suite to test file

--- testing/circle.py
+++ testing/circle.py
@@ -7,10 +7,10 @@ def circle_area(r):
 # print(circle_area(2))
 # Good check
 
-radii = [2, 0, -3, 2 + 5j, True, "string"]
-message = "Area of circles with r= {radius} is {area}"
+# radii = [2, 0, -3, 2 + 5j, True, "string"]
+# message = "Area of circles with r = {radius} is {area}"
 
-for r in radii:
-    A = circle_area(r)
-    print(message.format(radius=r, area=A))
+# for r in radii:
+#     A = circle_area(r)
+#     print(message.format(radius=r, area=A))
 
---
+++ testing/circle_test.py
@@ -0,0 +1,4 @@
+import unittest
+from circle import circle_area
+from math import pi
+
