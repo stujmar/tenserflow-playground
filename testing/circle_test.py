8308f1f1cbea2863c5b5c1fe99321e1afeb77a5d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-07 21:00:55 -0500

    [sm] wrote four test cases for simple positive radii

--- testing/circle_test.py
+++ testing/circle_test.py
@@ -3,4 +3,8 @@ from circle import circle_area
 from math import pi
 
 class TestCircleArea(unittest.TestCase):
-    
+    def test_area(self):
+        # Test areas of positive radii
+        self.assertAlmostEqual(circle_area(1), pi)
+        self.assertAlmostEqual(circle_area(0), pi)
+        self.assertAlmostEqual(circle_area(2.1), pi * 2.1**2)
